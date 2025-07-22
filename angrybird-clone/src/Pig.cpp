b6c4e595b26930310859f476a09d6b63010072d7
Sparsha Bhusal <50956733+bhusalsparsha@users.noreply.github.com>
2025-07-22 14:30:41 +0545

    Update Pig.cpp
    Added sound effect code.

--- angrybird-clone/src/Pig.cpp
+++ angrybird-clone/src/Pig.cpp
@@ -1,30 +1,57 @@
-// Pig.cpp
-#include "Pig.hpp"
+// // Pig.cpp
+// #include "Pig.hpp"
 
-Pig::Pig(float x, float y) :
-    alive(true)
-{
-    shape.setRadius(15.0f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setOrigin(shape.getRadius(), shape.getRadius());
-    shape.setPosition(x, y);
-}
+// Pig::Pig(float x, float y) :
+//     alive(true)
+// {
+//     shape.setRadius(15.0f);
+//     shape.setFillColor(sf::Color::Green);
+//     shape.setOrigin(shape.getRadius(), shape.getRadius());
+//     shape.setPosition(x, y);
+// }
 
-void Pig::draw(sf::RenderWindow& window) {
-    if (alive) {
-        window.draw(shape);
-    }
-}
+// void Pig::draw(sf::RenderWindow& window) {
+//     if (alive) {
+//         window.draw(shape);
+//     }
+// }
 
-sf::FloatRect Pig::getBounds() const {
-    return shape.getGlobalBounds();
-}
+// sf::FloatRect Pig::getBounds() const {
+//     return shape.getGlobalBounds();
+// }
 
-bool Pig::isAlive() const {
-    return alive;
-}
+// bool Pig::isAlive() const {
+//     return alive;
+// }
 
-void Pig::hit() {
-    alive = false;
-    // Optionally change color or add explosion effect here
-}
+// void Pig::hit() {
+//     alive = false;
+//     // Optionally change color or add explosion effect here
+// }
+
+//Sound effect added 
+// Pig.hpp
+#ifndef PIG_HPP
+#define PIG_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+class Pig {
+public:
+    Pig(float x, float y);
+
+    void draw(sf::RenderWindow& window);
+    sf::FloatRect getBounds() const;
+    bool isAlive() const;
+    void hit(); // Mark pig as hit/destroyed and play sound
+
+private:
+    sf::CircleShape shape;
+    bool alive;
+
+    sf::SoundBuffer hitBuffer;
+    sf::Sound hitSound;
+};
+
+#endif // PIG_HPP
